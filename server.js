ecc55e2f8bc0fc62c8167ad6963cb640c16f7382
Tucker Vassau <tuckv@hotmail.com>
2020-03-04 21:24:28 -0700

    Fixed!

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+// var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
