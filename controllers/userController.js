81d55eddb4559cf508f94565255482126069c3e9
Tyler Snyder <tyler@tylers-mbp.uguest.utah.edu>
2020-03-04 21:48:51 -0700

    setup home and portal route

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,4 +4,14 @@ var router = express.Router();
 
 // var user = require("../models/user.js");
 
+// Home page route
+router.get("/", function(req, res) {
+  res.render("landing");
+});
+
+// Portal page route
+router.get("/portal", function(req, res) {
+  res.render("portal");
+});
+
 module.exports = router;
