082f25f8d4b92ea367f41a4160cd35c64ada2f59
Tyler Snyder <tyler@tylers-mbp.uguest.utah.edu>
2020-03-07 13:11:56 -0700

    fixed login auth

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: "passport_loginDB",
+    database: "soggy_bottomDB",
     host: "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
