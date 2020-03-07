56bf8d28b7f0a13b12ef253acc6708458d42a9bd
Tyler Snyder <tyler@tylers-mbp.uguest.utah.edu>
2020-03-07 10:02:59 -0700

    set up some routes and added jquery true

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
+const passport = require("../config/passport");
+
 // var user = require("../models/user.js");
 
 // Home page route
@@ -9,10 +11,29 @@ router.get("/", function(req, res) {
   res.render("landing");
 });
 
-// signup page route
+router.post("/api/login", passport.authenticate("local"), function(req, res) {
+  res.json(req.user);
+});
+
+// signup page get route
 router.get("/signup", function(req, res) {
   res.render("signup");
 });
+
+// router.post("/api/signup", function(req, res) {
+//   db.User.create({
+//     email: req.body.email,
+//     password: req.body.password
+//   })
+//     .then(() => {
+//       res.redirect(307, "/api/login");
+//     })
+//     .catch(err => {
+//       res.status(401).json(err);
+//     });
+// });
+
+router.post("/");
 
 // Portal page route
 router.get("/portal", function(req, res) {
